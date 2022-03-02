989212a9a79b38b4a32102326a8c73b77cbb9f3b
snehakpersistent <Sneha.Kanekar1@ibm.com>
2022-03-02 15:10:45 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,11 +7,6 @@ git checkout tags/$(git describe --tags --abbrev=0)
 python3 -m venv cryptography-venv
 pip3 install --upgrade pip
 pip3 install --editable .
-pip3 install wheel
-pip3 install setuptools_rust
-
-sh <(curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs) -y
-source ~/.bashrc
 
 pip3 install --requirement dev-requirements.txt
 python3 ./setup.py build
